84538724d14fac9fd2a704f1798a668f408048f2
JanSefzig <nicolas.jan.sefzig@student.ossp.cz>
2022-11-16 12:50:35 +0100

    k

---
+++ py.py
@@ -0,0 +1,67 @@
+
+import turtle
+from turtle import *
+
+ws = Screen()
+ws.setup(width=400, height=400)
+
+for s in range(4):
+    left(90)
+    forward(150)    
+
+backward(30)
+left(90)
+
+for s in range(4):
+    forward(100)  
+    left(90)
+
+done() 
+
+
+import turtle
+from turtle import *
+
+ws = Screen()
+ws.setup(width=400, height=400)
+
+for s in range(4):
+    left(90)
+    forward(150) 
+
+backward(45)
+left(45)
+forward(100)  
+
+for s in range(3):
+    left(90)
+    forward(150) 
+
+left(90)
+forward(50)
+done() 
+
+
+
+import turtle
+from turtle import *
+
+ws = Screen()
+ws.setup(width=400, height=300)
+
+for s in range(4):
+    left(90)
+    forward(150)    
+
+left(90)
+forward(75)
+right(90)
+forward(75)
+
+for s in range(3):
+    left(80)
+    forward(140)   
+
+left(80)
+forward(80)
+done()
